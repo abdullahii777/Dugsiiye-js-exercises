82ba5de1d7de8610ad4e721b2d1093aaee5a28da
abdullahii777 <apdalaisse1@gmail.com>
2026-04-20 17:59:09 +0300

    Submitted exercise 2

---
+++ Exercise 2/ex-2.js
@@ -0,0 +1,27 @@
+
+let a = 15;
+let b = "15";
+
+// Equal to
+console.log(a == b);
+
+// Not equal to
+console.log(a != b);
+
+// Strict equal to
+console.log(a === b);
+
+// Strict not equal to
+console.log(a !== b);
+
+// Greater than
+console.log(a > b);
+
+// Less than
+console.log(a < b);
+
+// Greater than or equal to
+console.log(a >= b);
+
+// Less than or equal to
+console.log(a <= b);
