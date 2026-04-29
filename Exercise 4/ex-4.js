73bbcf22e463c1c98f936b674f63d627279774fc
abdullahii777 <apdalaisse1@gmail.com>
2026-04-29 07:45:13 +0300

    submitted exercise 4

---
+++ Exercise 4/ex-4.js
@@ -0,0 +1,16 @@
+// function basics.
+
+function add(x, y) {
+    console.log(x + y);
+}
+add(5, 10);
+add(20, 30);
+add(100, 200);
+
+// /Function expressions
+const add2 = function (x, y) {
+    console.log(x + y);
+};
+add2(5, 10);
+add2(20, 30);
+add2(100, 200);
